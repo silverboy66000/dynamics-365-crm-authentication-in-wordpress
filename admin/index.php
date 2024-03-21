e45922de556fe8e119ae4ead8c220e78176ff121
a.samiei <p.abolfazl.samiei@gmail.com>
2024-03-21 23:16:51 +0330

    feat improve code

--- admin/index.php
+++ admin/index.php
@@ -1,182 +1,204 @@
 <?php
 global $wpdb;
-$table_name = $wpdb->prefix . 'wp_product_logs_list_settings';
+global $wp_roles;
+require_once( ABSPATH . 'wp-includes/http.php' );
 
-$exampleListTable = new List_Table();
-$exampleListTable->prepare_items();
+$noyan_crm_settings = $wpdb->prefix . 'noyan_crm_settings';
+$roles = $wp_roles->get_names();
+$user = wp_get_current_user();
+$userid=$user->ID;
+$http = _wp_http_get_object();
+
+
+
+$productArgs = array(
+	'posts_per_page' => -1,
+	'post_type'      => 'product',
+	'post_status'    => 'publish',
+);
+
+$categoryArgs = array(
+	'taxonomy'     => 'product_cat',
+	'hide_empty'     => 0,
+);
+
+
+$loopProducts = new WP_Query( $productArgs );
+$all_categories = get_categories( $categoryArgs );
 
 ?>
 
 <div class="wrap">
-	<div id="icon-users" class="icon32"></div>
-	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-	<?php
-	$exampleListTable->search_box('جستجو', 'search_id');
-	$exampleListTable->display();
-	?>
+    <div id="icon-users" class="icon32"></div>
+    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 </div>
-
 <?php
-
-if( ! class_exists( 'WP_List_Table' ) ) {
-	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
-}
-
-class List_Table extends WP_List_Table
+if (isset($_POST['client_id']))
 {
+	$Args = array(
+		'client_id' => $_POST['client_id'],
+		'resource' => $_POST['resource'],
+		'username' => $_POST['username'],
+		'password' => $_POST['code'],
+		'client_secret' => $_POST['client_secret'],
+		'grant_type' => $_POST['grant_type'],
+	);
+	$response= $http->post( 'https://sts.eversso.com/adfs/oauth2/token', array(
+		'body'    => $Args,
+		'headers' => array(
+			'Content-Type' => 'application/x-www-form-urlencoded',
+		),
+	)
+    );
 
-	public function prepare_items()
+	$responseData=json_decode($response['body'],true);
+	$accessToken=$responseData['access_token'];
+	$jwt=json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $accessToken)[1]))));
+
+	$expToken=$jwt->exp;
+
+
+
+
+	$sql = "SELECT * FROM  $noyan_crm_settings where id=1";
+	$results = $wpdb->get_results($sql);
+	if ($results==null)
 	{
-        require_once ( ABSPATH .'wp-content/plugins/product-list-management/public/asset/jdf.php');
-        $columns = $this->get_columns();
-		$hidden = $this->get_hidden_columns();
-		$sortable = $this->get_sortable_columns();
-
-		$data = $this->table_data();
-		usort( $data, array( &$this, 'sort_data' ) );
-
-		$perPage = 25;
-		$currentPage = $this->get_pagenum();
-		$totalItems = count($data);
-
-		$this->set_pagination_args( array(
-			'total_items' => $totalItems,
-			'per_page'    => $perPage
-		) );
-
-		$data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
-
-		$this->_column_headers = array($columns, $hidden, $sortable);
-		$this->items = $data;
-	}
-
-	public function get_columns()
-	{
-		$columns = array(
-			'id'          => 'ID',
-			'productId'       => 'نام محصول',
-			'price'       => 'قیمت',
-			'inventory'       => 'تعداد',
-			'updateDate'       => 'وضعیت در انبار',
-			'userId'        => 'نام ویرایشگر',
-			'date'        => 'تاریخ ویرایش',
-		);
-
-		return $columns;
-	}
-
-	public function get_hidden_columns()
-	{
-		return array();
-	}
-
-	public function get_sortable_columns()
-	{
-		return array(
-			'id' => array('id', false),
-			'productId' => array('productId', false),
-			'price' => array('price', false),
-			'inventory' => array('inventory', false),
-            'updateDate' => array('updateDate', false),
-            'userId' => array('userId', false),
-			'date' => array('date', false)
+		$wpdb->insert($noyan_crm_settings, array(
+				'client_id' => $_POST['client_id'],
+				'resource' => $_POST['resource'],
+				'username' => $_POST['username'],
+				'code' => $_POST['code'],
+				'client_secret' => $_POST['client_secret'],
+				'grant_type' => $_POST['grant_type'],
+				'exp' => $expToken,
+				'access_token' => $accessToken,
+			)
 		);
 	}
-
-	private function table_data()
+	else
 	{
-		$data = array();
-		global $wpdb;
-
-		$table_name = $wpdb->prefix . 'product_logs_list_settings';
-		$sql = "SELECT * FROM  $table_name ORDER BY id DESC";
-		$results = $wpdb->get_results($sql);
-		if (count($results) > 0) {
-			foreach ($results as $res) {
-				$data[] = array(
-					'id'          => $res->id,
-					'productId'    => $this->column_product($res->productId),
-					'price'        => number_format($res->price),
-					'inventory'     => $res->inventory,
-					'updateDate'     => $this->column_title($res->updateDate)  ,
-					'userId'       => $this->get_userdata($res->userId),
-					'date'       => jdate("Y/m/d H:i:s", strtotime($res->date)),
-				);
-			}
-		}
-
-		return $data;
+        $wpdb->update($noyan_crm_settings,
+			array(
+				'client_id' => $_POST['client_id'],
+				'resource' => $_POST['resource'],
+				'username' => $_POST['username'],
+				'code' => $_POST['code'],
+				'client_secret' => $_POST['client_secret'],
+				'grant_type' => $_POST['grant_type'],
+				'exp' => $expToken,
+				'access_token' => $accessToken,
+			),
+			array(
+				'id'=>1
+			)
+		);
 	}
-    function get_userdata( $user_id ) {
-
-        return get_user_meta($user_id)['first_name'][0].' '.get_user_meta($user_id)['last_name'][0];
-    }
-    function column_title($item) {
-
-        if ($item=='outofstock')
-        {
-            return 'ناموجود شده';
-        }
-        else
-        {
-            return 'موجود درانبار';
-        }
-    }
-    function column_product($item)
-    {
-        $product = wc_get_product( $item );
-        return $product->get_title();
-    }
-	public function column_default( $item, $column_name )
-	{
-		switch( $column_name ) {
-			case 'id':
-			case 'productId':
-			case 'price':
-			case 'inventory':
-			case 'updateDate':
-			case 'userId':
-			case 'date':
-				return $item[ $column_name ];
-
-			default:
-				return print_r( $item, true ) ;
-		}
-	}
-
-	private function sort_data( $a, $b )
-	{
-		// Set defaults
-		$orderby = 'id';
-		$order = 'desc';
-
-		// If orderby is set, use this as the sort column
-		if(!empty($_GET['orderby']))
-		{
-			$orderby = $_GET['orderby'];
-		}
-
-		// If order is set use this as the order
-		if(!empty($_GET['order']))
-		{
-			$order = $_GET['order'];
-		}
-
-
-		$result = strcmp( $a[$orderby], $b[$orderby] );
-
-		if($order === 'asc')
-		{
-			return $result;
-		}
-
-		return -$result;
-	}
+	?>
+    <div class="postbox">
+        <div class="inside">
+تنظیمات با موفقیت ذخیره شد!
+        </div>
+    </div>
+	<?php
 }
-
-
+$results=null;
+$sqlThreshold = "SELECT * FROM  $noyan_crm_settings where id=1";
+$results = $wpdb->get_row($sqlThreshold);
 
 ?>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+</script>
+<div class="wrap fsww">
+    <h2 class="nav-tab-wrapper">
+        <a href="#" class="nav-tab active">تنظیمات </a>
+    </h2>
 
+    <div class="postbox">
+        <div class="inside">
+            <form action="#" method="post">
+                <div class="input-box">
+                    <div class="label">
+                        <span>client_id</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="client_id" type="text" value="<?php echo $results ? $results->client_id : null ?>">
+                    </div>
+                </div>
+                <div class="input-box">
+                    <div class="label">
+                        <span>resource</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="resource" type="text" value="<?php echo $results ? $results->resource : null ?>">
+                    </div>
+                </div>
+                <hr>
+                <div class="input-box">
+                    <div class="label">
+                        <span>username</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="username" type="text" value="<?php echo $results ? $results->username : null ?>">
+                    </div>
+                </div>
+                <div class="input-box">
+                    <div class="label">
+                        <span>password</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="code" type="password" value="<?php echo $results ? $results->code : null ?>">
+                    </div>
+                </div>
+
+                <div class="input-box">
+                    <div class="label">
+                        <span>client_secret</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="client_secret" type="text" value="<?php echo $results ? $results->client_secret : null ?>">
+                    </div>
+                </div>
+
+                <div class="input-box">
+                    <div class="label">
+                        <span>grant_type</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="grant_type" type="text" value="<?php echo $results ? $results->grant_type : null ?>">
+                    </div>
+                </div>
+
+                <div class="input-box">
+                    <div class="label">
+                        <span>exp</span>
+                    </div>
+                    <div class="input">
+                        <input class="input-field" name="exp" type="text" value="<?php echo $results ? $results->exp : null ?>">
+                        <?php echo wp_date( 'Y/m/d - h:i:s', $results->exp, new DateTimeZone('Asia/Tehran') )?>
+                    </div>
+                </div>
+
+                <div class="input-box">
+                    <div class="label">
+                        <span>access token</span>
+                    </div>
+                    <div class="input">
+                        <textarea class="input-field" style="width:100%" rows="10" type="text"><?php echo $results ? $results->access_token : null ?></textarea>
+                    </div>
+                </div>
+                <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="ذخیرهٔ تغییرات"></p>
+            </form>
+        </div>
+    </div>
+
+</div>
 
